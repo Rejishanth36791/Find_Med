e0c2b0c1519785c154695582a6805bfd48ba96d4
P.Rejishanth <rejishanth422@gmail.com>
2026-09-25 19:16:13 +0530

    fix(backend): remove unnecessary deprecation warning annotation

--- backend/src/main/java/com/findmymeds/backend/config/ApplicationConfig.java
+++ backend/src/main/java/com/findmymeds/backend/config/ApplicationConfig.java
@@ -45,7 +45,6 @@ public class ApplicationConfig {
     }
 
     @Bean
-    @SuppressWarnings("deprecation")
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService());
